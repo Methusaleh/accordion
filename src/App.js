eb1eb272d7335b5441e939647d78d45eef6daffe
Aaron Kipf <samplethis84@gmail.com>
2026-02-09 13:03:36 -0600

    finished accordion

--- src/App.js
+++ src/App.js
@@ -25,34 +25,54 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((faq, index) => (
         <AccordionItem
           title={faq.title}
-          text={faq.text}
           num={index}
           key={faq.title}
-        />
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+        >
+          {faq.text}
+        </AccordionItem>
       ))}
+
+      <AccordionItem
+        title="Test 1"
+        num={22}
+        key="Test 1"
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+      >
+        <p>Allows React developers to:</p>
+        <ul>
+          <li>Break up UI into components</li>
+          <li>Make components reusuable</li>
+          <li>Place state efficiently</li>
+        </ul>
+      </AccordionItem>
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = curOpen === num;
 
   function handleToggle() {
-    setIsOpen((isOpen) => !isOpen);
+    onOpen(isOpen ? null : num);
   }
 
   return (
-    <div className="title" onClick={handleToggle}>
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
 
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
